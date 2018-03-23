05fc2720c36f4e1cfc63749f9d7a6f1549cb2730
GA1 <nvkga1@gmail.com>
2018-03-23 15:08:23 +0100

    Add redux and axios to frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!');
 });
